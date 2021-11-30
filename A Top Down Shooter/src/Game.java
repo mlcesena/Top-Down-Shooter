f36bce5843a0219e1d05c2ea9cf320bb54d8cdfd
Tyler Battershell <TylerBattershell@yahoo.com>
2021-11-30 14:04:20 -0500

    Michael help

--- A Top Down Shooter/src/Game.java
+++ A Top Down Shooter/src/Game.java
@@ -145,6 +145,7 @@ public class Game extends Canvas implements Runnable {
 		assetController.render(g);
 
 		g2d.translate(camera.getX(), camera.getY());
+		//Change for Michael
 
 		/* ========================================== */
 		/* ^ DRAW HERE ^ */
