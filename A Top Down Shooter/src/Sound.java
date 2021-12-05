dd26a8a4495ddec5894ccd265f9d2b580f8a43d1
Tyler Battershell <TylerBattershell@yahoo.com>
2021-12-05 12:36:50 -0500

    Comments

--- A Top Down Shooter/src/Sound.java
+++ A Top Down Shooter/src/Sound.java
@@ -1,10 +1,21 @@
 import java.io.File;
 import javax.sound.sampled.*;
 
+/**
+ * Sound class enables us to create music and sound effects for our game through Java's imported libraries.
+ * 
+ * @author Tyler Battershell
+ */
 public class Sound {
     
     Clip clip;
 
+    /**
+     * Overloading constructor to create a sound clip from the given file name.
+     * Uses AudioInputStream and CLip from Java library.
+     * 
+     * @param name - File name (.wav)
+     */
     public Sound (String name) {
         try {
             String fullPath = System.getProperty("user.dir") + "/src/sounds/" + name;
@@ -15,6 +26,10 @@ public class Sound {
             System.out.println("Exception: " + e);
         }
     }
+
+    /**
+     * start() method starts a clip and closes it after it is done running.
+     */
     public void start() {
         clip.start();
         while (clip.isRunning()) {
@@ -22,9 +37,17 @@ public class Sound {
 			    clip.close();
         }
     }
+
+    /**
+     * loop() method starts a clip and continues to loop that clip indefinitely.
+     */
     public void loop() {
         clip.loop(Clip.LOOP_CONTINUOUSLY);
     }
+
+    /**
+     * reset() method stops a playing clip and sets that clip back to the beginning.
+     */
     public void reset() {
         clip.stop();
         clip.setMicrosecondPosition(0);
